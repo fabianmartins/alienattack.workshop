c07012aae49ad9029eab43885adedefc74e6edef
Da Silva <fabisilv@88e9fe67a9c5.ant.amazon.com>
2019-03-13 03:27:56 -0400

    makeup .sh

--- config.sh
+++ config.sh
@@ -1,16 +1,17 @@
 echo CONFIGURING THE ENVIRONMENT
-echo
+echo #############################
 echo Updating the attached instance
 sudo yum update -y
-echo
+echo --
 echo Updating node to the latest version
 nvm install --lts
-echo 
+echo --
 echo Installing CDK
 npm install -g aws-cdk
-echo
+echo --
 echo Installing Typescript
 npm install -g typescript
-echo
+echo --
 echo Installing dependencies
 npm install
+echo ### DONE
