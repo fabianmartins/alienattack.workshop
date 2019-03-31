b3dfcd7d3d9506055e070e274ea75c881c05869e
Da Silva <fabisilv@88e9fe67a9c5.ant.amazon.com>
2019-03-30 21:28:33 -0400

    removing *.d.ts files

--- cdk/lib/layer/databaseLayer.d.ts
+++
@@ -1,7 +0,0 @@
-import { Construct } from '@aws-cdk/cdk';
-import { ResourceAwareConstruct, IParameterAwareProps } from './../resourceawarestack';
-import DynamoDB = require('@aws-cdk/aws-dynamodb');
-export declare class DatabaseLayer extends ResourceAwareConstruct {
-    tables: Map<string, DynamoDB.Table>;
-    constructor(parent: Construct, name: string, props: IParameterAwareProps);
-}
--- cdk/lib/layer/mainLayer.d.ts
+++
@@ -1,6 +0,0 @@
-import { App } from '@aws-cdk/cdk';
-import { IParameterAwareProps, ResourceAwareStack } from '../resourceawarestack';
-export declare class MainLayer extends ResourceAwareStack {
-    constructor(scope: App, id: string, props?: IParameterAwareProps);
-    buildResources(): void;
-}
