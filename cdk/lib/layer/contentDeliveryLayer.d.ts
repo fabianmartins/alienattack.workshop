0103503ad50bd3c567862a2da3fd40b48b6dca71
Da Silva <fabisilv@88e9fe67a9c5.ant.amazon.com>
2019-03-30 22:08:12 -0400

    removing .d.ts

--- cdk/lib/layer/contentDeliveryLayer.d.ts
+++
@@ -1,6 +0,0 @@
-import { Construct } from '@aws-cdk/cdk';
-import { ResourceAwareConstruct, IParameterAwareProps } from './../resourceawarestack';
-export declare class ContentDeliveryLayer extends ResourceAwareConstruct {
-    constructor(parent: Construct, name: string, props: IParameterAwareProps);
-    private createDistribution;
-}
--- cdk/lib/nrta.d.ts
+++
@@ -1,5 +0,0 @@
-import { ParameterAwareProps, IParameterAwareProps } from '../lib/resourceawarestack';
-export declare class NRTAProps extends ParameterAwareProps {
-    constructor(props?: IParameterAwareProps);
-    getBucketNames(): string[];
-}
