877bd331e7a51b5a16dae534f823ace48e10d6bd
EC2 Default User <ec2-user@ip-172-31-35-112.us-east-2.compute.internal>
2019-12-31 13:57:50 +0000

    Updating contentDeliveryLayer.ts to comply with the changes introduced by https://github.com/aws/aws-cdk/pull/4491

--- cdk/lib/layer/contentDeliveryLayer.ts
+++ cdk/lib/layer/contentDeliveryLayer.ts
@@ -1,6 +1,6 @@
 import { Construct } from '@aws-cdk/core';
 import { ResourceAwareConstruct, IParameterAwareProps } from './../resourceawarestack'
-import { CloudFrontWebDistribution, CfnCloudFrontOriginAccessIdentity } from '@aws-cdk/aws-cloudfront';
+import { CloudFrontWebDistribution, CfnCloudFrontOriginAccessIdentity, OriginAccessIdentity } from '@aws-cdk/aws-cloudfront';
 import { Bucket, BucketPolicy} from '@aws-cdk/aws-s3';
 import { PolicyStatement, ArnPrincipal } from '@aws-cdk/aws-iam';
 
@@ -17,39 +17,22 @@ export class ContentDeliveryLayer extends ResourceAwareConstruct {
         let s3BucketOrCnfBucket = props.getParameter('appBucket');
         let appBucket = <Bucket> Bucket.fromBucketName(this, props.getApplicationName()+'ImportedBucket', s3BucketOrCnfBucket.bucketName);
         
-        let cloudFrontAccessIdentity = new CfnCloudFrontOriginAccessIdentity(this,this.properties.getApplicationName()+'CDNAccessId', {
-            cloudFrontOriginAccessIdentityConfig : {
-                // This is the name of the identity
-                comment : this.properties.getApplicationName()+'CDNAccessId'
-            }
-        })
-
-        let bucketPolicy = new BucketPolicy(this, this.properties.getApplicationName()+"AppBucketPolicy", {
-            bucket : appBucket
-        });
+        let cloudFrontAccessIdentity = new OriginAccessIdentity(this,this.properties.getApplicationName()+'CDNAccessId');
+        s3BucketOrCnfBucket.grantRead(cloudFrontAccessIdentity);
+            
         
-        bucketPolicy.document.addStatements(
-            new PolicyStatement({
-                resources : [  appBucket.bucketArn+'/*' ],
-                actions : [ 's3:GetObject' ],
-                principals : [
-                    new ArnPrincipal('arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity '+cloudFrontAccessIdentity.ref) //cloudFrontAccessIdentity.cloudFrontOriginAccessIdentityConfig)
-                ]
-            })
-        );
-
-        let distribution = new CloudFrontWebDistribution(this, props.getApplicationName(), {
-            originConfigs: [
+        let distribution = new CloudFrontWebDistribution(this, props.getApplicationName(),{
+            originConfigs : [
                 {
-                    s3OriginSource: {
-                         s3BucketSource: appBucket,
-                         originAccessIdentityId : cloudFrontAccessIdentity.ref
+                    s3OriginSource : {
+                        s3BucketSource: appBucket,
+                        originAccessIdentity : cloudFrontAccessIdentity
                     },
                     behaviors : [ {isDefaultBehavior: true}]
                 }
             ]
-         });
+        });
 
-         this.addResource("cdndomain",distribution.domainName);
+        this.addResource("cdndomain",distribution.domainName);
     }
 }
